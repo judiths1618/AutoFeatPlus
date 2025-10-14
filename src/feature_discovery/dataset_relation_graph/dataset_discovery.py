84e48c5173b27c0b8f59dfe9b3701aa7ff584778
judith1618 <judiths11@outlook.com>
2025-10-14 16:02:30 +0200

    check the results

--- src/feature_discovery/dataset_relation_graph/dataset_discovery.py
+++ src/feature_discovery/dataset_relation_graph/dataset_discovery.py
@@ -52,9 +52,9 @@ def profile_valentine_logic(files: List[str], valentine_threshold: float = 0.55)
         # matcher = Coma(use_instances = True, java_xmx = "4g") # use_instances=True enables instance-based matching
         # matcher = Coma()  # COMA matcher
         # matcher = JaccardDistanceMatcher()  # Jaccard distance matcher
-        matcher = SimilarityFlooding()  # Similarity flooding matcher
+        # matcher = SimilarityFlooding()  # Similarity flooding matcher
         # matcher = Cupid()  # Cupid matcher
-        # matcher = DistributionBased()  # Distribution-based matcher
+        matcher = DistributionBased()  # Distribution-based matcher
 
         # print(matcher)
         matches = valentine_match(df1, df2, matcher)
